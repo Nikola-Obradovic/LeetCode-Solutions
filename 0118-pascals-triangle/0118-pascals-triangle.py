a9f65d4b0290b95c2a64116fac46cc3f7e2f6a87
Nikola Obradovic <113454455+Nikola-Obradovic@users.noreply.github.com>
2023-07-30 04:26:07 +0200

    Time: 46 ms (74.63%), Space: 16.4 MB (43.85%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -0,0 +1,26 @@
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        templist=[1]
+        templist2=[]
+        result=[[1]]
+        i=0
+        while i<numRows:
+            for x in range(len(templist)):
+                if x==0:
+                    templist2.append(1)
+                
+                if len(templist)>1 and x>0:
+                    templist2.append(templist[x]+templist[x-1])
+                if x==len(templist)-1:
+                    templist2.append(1)
+            
+            
+            result.append(templist2)
+            templist=templist2
+            templist2=[]
+            i+=1
+        result.pop()
+        return result
+
+
+        

4a869ee2b757575bd2d32261b94389c4157e27a0
Nikola Obradovic <113454455+Nikola-Obradovic@users.noreply.github.com>
2023-08-12 02:06:20 +0200

    Time: 170 ms (5.01%), Space: 16.7 MB (30.1%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,5 +1,8 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        if ransomNote in magazine:
+            return True
+
         for i in range(len(ransomNote)):
             if not ransomNote[i] in magazine:
                 return False

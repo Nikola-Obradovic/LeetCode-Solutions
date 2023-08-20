ece33e275c01540b3bbffd791bd6eaba07362a65
Nikola Obradovic <113454455+Nikola-Obradovic@users.noreply.github.com>
2023-08-20 04:36:21 +0200

    Time: 42 ms (73.43%), Space: 16.2 MB (77.02%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.py
@@ -0,0 +1,22 @@
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        char_count = {}
+        
+        for char in s:
+            char_count[char] = char_count.get(char, 0) + 1
+        
+        palindrome_length = 0
+        odd_found = False
+        
+        for count in char_count.values():
+            if count % 2 == 0:
+                palindrome_length += count
+            else:
+                palindrome_length += count - 1
+                odd_found = True
+        
+        if odd_found:
+            palindrome_length += 1
+        
+        return palindrome_length
+        

c1c19f58be84723dd7201ba87d3dca32130f9b95
Nikola Obradovic <113454455+Nikola-Obradovic@users.noreply.github.com>
2023-09-16 05:50:51 +0200

    Time: 36 ms (85.84%), Space: 16.1 MB (99.54%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.py
+++ 0075-sort-colors/0075-sort-colors.py
@@ -1,5 +1,30 @@
 class Solution:
-    def sortColors(self, nums: List[int]) -> None:
-        nums.sort()
+    def Partition(self, nums, low, high):
+        pivot = nums[high]
+        i=low-1
+        for j in range(low, high):
+            if nums[j]<pivot:
+                i+=1
+                temp=nums[i]
+                nums[i]=nums[j]
+                nums[j]=temp
+        i+=1
+        temp=nums[i]
+        nums[i]=nums[high]
+        nums[high]=temp
         
+        return i
 
+    def quickSort(self, nums, low, high):
+        if high<=low:
+            return
+        pivot=self.Partition(nums,low,high)
+        self.quickSort(nums,low,pivot-1)
+        self.quickSort(nums,pivot+1,high)
+
+    def sortColors(self, nums: List[int]) -> None:
+        low=0
+        high=len(nums)-1
+
+        self.quickSort(nums,low,high)
+     

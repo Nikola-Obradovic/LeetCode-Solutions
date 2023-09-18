f4caa626f1e890231e2af6964764aaf33df32b27
Nikola Obradovic <113454455+Nikola-Obradovic@users.noreply.github.com>
2023-09-19 00:30:19 +0200

    Time: 39 ms (69.12%), Space: 16.2 MB (67.68%) - LeetHub

---
+++ 0343-integer-break/0343-integer-break.py
@@ -0,0 +1,11 @@
+class Solution:
+    def integerBreak(self, n: int) -> int:
+        if n==2 or n==3:
+            return n-1
+        
+        res=1
+        while n>4:
+            n-=3
+            res*=3
+        
+        return n*res

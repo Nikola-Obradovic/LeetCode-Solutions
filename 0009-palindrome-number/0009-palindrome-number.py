8194758d7a7d294701b2a8dcfdfa4ae032488750
Nikola Obradovic <113454455+Nikola-Obradovic@users.noreply.github.com>
2023-09-24 04:21:55 +0200

    Time: 52 ms (23.82%), Space: 13.1 MB (78.31%) - LeetHub

---
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        if x<0:
+            return False
+        
+        y=str(x)
+        i=0
+        j=len(y)-1
+        while i<j:
+            if y[i]!=y[j]:
+                return False
+            i+=1
+            j-=1
+        return True
+
+
+
+
+        

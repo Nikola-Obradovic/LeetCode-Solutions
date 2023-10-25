92b025f0f079bdbf83345a65c22cab3570d423db
Nikola Obradovic <113454455+Nikola-Obradovic@users.noreply.github.com>
2023-10-25 23:13:59 +0200

    Time: 47 ms (6.95%), Space: 16.4 MB (17.63%) - LeetHub

--- 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
+++ 0795-k-th-symbol-in-grammar/0795-k-th-symbol-in-grammar.py
@@ -2,7 +2,7 @@ class Solution:
     def kthGrammar(self, n: int, k: int) -> int:
         def f(k):
             if k==1: return 0
-            b=(int)(math.log2(k)) # same as b=k.bit_length()-1
+            b=(int)(math.log2(k))
             if k==1<<b: return b%2
             else: return 1-f(k-(1<<b))
         return f(k)
